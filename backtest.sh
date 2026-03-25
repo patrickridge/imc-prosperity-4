e5cb5ca1f7ec8f95d0dfc595c77c951761178bcf
Lev <l.rozanov@outlook.com>
2026-03-25 10:54:17 +0000

    Readjust structure

--- backtest.sh
+++ backtest.sh
@@ -3,4 +3,4 @@
 #        ./backtest.sh strategies/my_strat.py 0--2    (round 0, day -2 only)
 #        ./backtest.sh strategies/my_strat.py 0 --vis  (open visualizer)
 set -e
-python3 -m backtester "$@"
+python3 -m backtester "$@" --data data/
